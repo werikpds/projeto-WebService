70d3b87540e4050f52f9088ddbfc8f5b9620d0d0
Werik Pinheiro <werikpds@gmail.com>
2019-10-21 20:14:13 -0300

    Segundo commit

---
+++ src/main/java/com/br/projetowebservice/model/beans/Usuario.java
@@ -0,0 +1,52 @@
+package com.br.projetowebservice.model.beans;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table (name = "usuario")
+public class Usuario implements Serializable{
+	private static final long serialVersionUID = 1l;
+	@Id
+	private int id;
+	private String nome;
+	private String email;
+	private String cpf;
+	private String senha;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	
+	
+}
